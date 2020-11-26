9721cfcbc77d94b8e74b486575b028f9730940d3
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-11-26 13:25:52 +0100

    New class added 'Book'

---
+++ Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooklistRazor.Model
+{
+    public class Book
+    {
+       [Key]               // This is a data annotation
+        public int Id { get; set; }
+
+        [Required]        // display attribute. Means 'Name' cannot be NULL
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
