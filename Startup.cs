99332e14edc5ad3198a100c813475f80493fc083
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-11-26 08:38:51 +0100

    add nuget package and modifiy startup.cs

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace BooklistRazor
         // HERE WE CAN ADD SERVICES
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
