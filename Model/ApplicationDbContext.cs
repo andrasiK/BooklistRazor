7b05823389b99e1ac52d9614792ff07b1ff6e9ac
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-11-27 08:22:15 +0100

    Database created with Book model in it

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooklistRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Constructor with pass the DbContext options passing to the parent class
+        // This is an empty constructor, but the parameter is needed to dependency injection
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; } 
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooklistRazor.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace BooklistRazor
         // HERE WE CAN ADD SERVICES
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));  
+
+            // .AddRazorRuntimeCompilation()  is added later with NuGet packages - to enable refreshing UI while program running
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
